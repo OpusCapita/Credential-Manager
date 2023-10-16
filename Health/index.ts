1ad85a388f7acf56fc7ef6b7c3836124a286e9c8
Piotr <piotr.krzysztof.murdzia@gmail.com>
2023-10-16 14:03:59 +0200

    Revert "Merge branch 'stage'"
    This reverts commit 3cec346a3b6c275758ac54d7afda593e7363d40f, reversing
    changes made to 63f3b562e95d887d8f6bfcaa7bcfdceac4b9a27f.

--- Health/index.ts
+++ Health/index.ts
@@ -1,50 +1,12 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
-import { ErrorLogs } from "../_common/models/ErrorLogs.model";
-import { IPValidator } from "../_common/models/IPValidator.model";
-import { checkJWT } from "../_common/utils/CheckJWT.utils";
-
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest) {
-    let response: any = { // TODO: Add the correct response type
+    context.res = {
         status: 200,
         body: {
-            status: 'OK',
-            version: '1.0.1'
+            status: 'OK'
         }
     };
-
-    try {
-        const isValidIp = await IPValidator.isValidClientIP(req);
-
-        // Check if the client IP is in the allowed list
-        if (!isValidIp) {
-            const headers = req.headers;
-
-            const authorization = headers['authorization'] || '';
-
-            // Validate the JWT token
-            await checkJWT(authorization);
-        }
-    }
-    catch (error) {
-        console.log('---- error', error);
-
-        response = handleHealthError(error);
-    }
-
-    context.res = response;
-}
-
-const handleHealthError = (error: any) => {
-    ErrorLogs.insert(error, 'Unknown error', '-- Health Unknown error --');
-
-    return {
-        status: error.status ?? 500,
-        body: {
-            status: 'ERROR',
-            description: error.body.description ?? 'Sorry, but the service is temporarily unavailable. Please try again later.'
-        }
-    };
-}
+};
 
 export default httpTrigger;
