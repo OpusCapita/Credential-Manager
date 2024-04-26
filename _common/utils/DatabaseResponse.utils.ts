ed8e1cac2181cf47bd97cc5125994ca940c22a20
Piotr <piotr.krzysztof.murdzia@gmail.com>
2024-04-26 14:19:24 +0200

    file_name => field_name

--- _common/utils/DatabaseResponse.utils.ts
+++ _common/utils/DatabaseResponse.utils.ts
@@ -3,18 +3,18 @@ import { error } from "console";
 /**
  * Throws an error if a database resource already exists.
  * @param object - The object to check if it exists.
- * @param file_name - The name of the file associated with the resource.
+ * @param field_name - The name of the field associated with the resource.
  * @throws {Object} - An error object with status 409 and a body containing the conflict details.
  */
-export const throwIfDatabaseResourceExists = (object: any, file_name: string) => {
+export const throwIfDatabaseResourceExists = (object: any, field_name: string) => {
     if (object) {
         throw {
             status: 409,
             error: true,
             body: {
                 status: 'Conflict',
-                file_name: file_name,
-                description: `Resource with the provided ${file_name} already exists.`
+                field_name: field_name,
+                description: `Resource with the provided ${field_name} already exists.`
             },
             headers: {
                 'Content-Type': 'application/json'
@@ -26,17 +26,17 @@ export const throwIfDatabaseResourceExists = (object: any, file_name: string) =>
 /**
  * Throws an error if the database resource does not exist.
  * @param object - The object to check if it exists.
- * @param file_name - The name of the file associated with the resource.
+ * @param field_name - The name of the field associated with the resource.
  * @throws {Object} - An error object with status 404 and a description of the error.
  */
-export const throwIfDatabaseResourceNotExists = (object: any, file_name: string) => {
+export const throwIfDatabaseResourceNotExists = (object: any, field_name: string) => {
     if (!object) {
         throw {
             status: 404,
             error: true,
             body: {
                 status: 'Not Found',
-                description: `Resource with the provided ${file_name} does not exists.`
+                description: `Resource with the provided ${field_name} does not exists.`
             },
             headers: {
                 'Content-Type': 'application/json'
